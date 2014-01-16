f3ab9e85d758d9b9c9040fa421188dd54f98c864
Christopher Brown <io@henrian.com>
2014-01-16 01:19:38 -0600

    Add rotation among other fixes

--- eye/cli.py
+++ eye/cli.py
@@ -3,9 +3,10 @@ import sys
 import io
 import time
 from datetime import datetime
-# import sched
-# scheduler = sched.scheduler(time.time, time.sleep)
-# import tempfile
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 import picamera
 
@@ -14,33 +15,40 @@ from eye import s3
 
 
 def timelapse(parser):
-    parser.add_argument('--interval', type=int, default=60, help='# of seconds between pictures')
+    parser.add_argument('--interval', type=int, default=60,
+        help='# of seconds between pictures')
     parser.add_argument('--width', type=int, default=800, help='Image width')
     parser.add_argument('--height', type=int, default=600, help='Image height')
+    parser.add_argument('--rotation', type=int, choices=(0, 90, 180, 270), default=0,
+        help='Rotation (cable goes down)')
     opts = parser.parse_args()
 
     with picamera.PiCamera() as camera:
         camera.resolution = (opts.width, opts.height)
+        camera.rotation = opts.rotation
         camera.start_preview()
         # Camera warm-up time
         time.sleep(2)
         print >> sys.stderr, 'Camera warmed up'
 
         while True:
+            # timestamp is almost ISO-8601, except:
+            # no timezone, no milliseconds, and s/:/-/g
             timestamp = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
-            s3_path = 'images/%s/%s' % (opts.id, timestamp)
+            s3_path = 'images/%s/%s.jpg' % (opts.id, timestamp)
 
-            # fd, filepath = tempfile.mkstemp()
-            # with tempfile.TemporaryFile(suffix='.jpg') as fd:
             bytestream = io.BytesIO()
 
             # capture from pi camera into fd/filepath
-            print >> sys.stderr, 'Capturing  %s', s3_path
+            logger.debug('Capturing from Pi Camera')
             camera.capture(bytestream, 'jpeg')
-            print >> sys.stderr, 'Uploading %s', s3_path
+
+            # capture is in memory -- upload it
+            logger.info('Uploading %s', s3_path)
+            bytestream.seek(0)
             s3.upload(bytestream, s3_path)
 
-            print >> sys.stderr, 'Sleeping %ds', opts.interval
+            print >> sys.stderr, 'Sleeping %ds' % opts.interval
             time.sleep(opts.interval)
 
 
@@ -55,7 +63,11 @@ def main():
     parser.add_argument('--version', action='version', version=eye.__version__)
     opts, _ = parser.parse_known_args()
 
+    level = logging.DEBUG if opts.verbose else logging.INFO
+    logging.basicConfig(level=level)
+
     commands[opts.command](parser)
+
 
 if __name__ == '__main__':
     main()
--- eye/s3.py
+++ eye/s3.py
@@ -2,6 +2,16 @@ import sys
 from boto.s3.connection import S3Connection
 
 
+def human(units):
+    if units > 1.99e9:
+        return '%.2fG' % (units / 1e9)
+    if units > 1.99e6:
+        return '%.2fM' % (units / 1e6)
+    if units > 1.99e3:
+        return '%.2fK' % (units / 1e3)
+    return '%.2fK' % (units)
+
+
 def upload(fp, key, bucket='eye-pi'):
     '''New files go in a bucket called eye-pi, in the path:
 
@@ -13,7 +23,7 @@ def upload(fp, key, bucket='eye-pi'):
 
     def progress(complete, total):
         percentage = 100.00 * complete / total
-        sys.stderr.write('\r%s:%s >> S3 (%6.2f%%)', bucket, key, percentage)
+        sys.stderr.write('\r%s:%s >> S3 (%6.2f%% of %sB)' % (bucket, key, percentage, human(total)))
         sys.stderr.flush()
 
     s3_key.set_contents_from_file(fp, cb=progress, num_cb=100)
